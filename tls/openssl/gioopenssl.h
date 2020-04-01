054d86723333f440eb1133dde9286cf516d5ac0a
Ole André Vadla Ravnås <oleavr@gmail.com>
2020-04-01 19:00:06 +0200

    openssl: improve support for static linking

---
+++ tls/openssl/gioopenssl.h
@@ -0,0 +1,36 @@
+/*
+ * gioopenssl.h
+ *
+ * Copyright (C) 2015 NICE s.r.l.
+ *
+ * This file is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * In addition, when the library is used with OpenSSL, a special
+ * exception applies. Refer to the LICENSE_EXCEPTION file for details.
+ *
+ * Authors: Ignacio Casal Quinteiro
+ */
+
+#ifndef __GIOOPENSSL_H__
+#define __GIOOPENSSL_H__
+
+#include <glib.h>
+
+G_BEGIN_DECLS
+
+void g_io_module_openssl_register (void);
+
+G_END_DECLS
+
+#endif /* __GIOOPENSSL_H__ */
